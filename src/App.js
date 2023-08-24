53d0c13b8f580a6f4c1082236818fca5cfad96ac
Rajesh Somepalli <rs20add@herts.ac.uk>
2023-08-24 12:16:13 +0100

    pizza

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
        <header className="headercontent">
-         <Navbar Navitems={navbaritems}/> 
+         <Navbar/> 
        </header>
       
       <main className="maincontent">
